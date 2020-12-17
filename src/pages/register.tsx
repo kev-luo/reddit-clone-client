479c97433e4f6bedd8f569f47bacd06d1dbe1418
Kevin Luo <kvn.luo@gmail.com>
2020-12-17 18:47:11 -0500

    create wrapper component

--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -1,23 +1,31 @@
+import { FormErrorMessage, FormLabel, Input, FormControl } from "@chakra-ui/react";
 import { Formik, Form } from "formik";
+import { Wrapper } from "../components/Wrapper";
 interface registerProps {
 
 }
 
 const Register: React.FC<registerProps> = ({ }) => {
-  const initialValues = {username:"", password:""};
+  const initialValues = { username: "", password: "" };
 
   const handleSubmit = (values: { username: string; password: string; }) => {
     console.log(values)
   }
-  
+
   return (
-    <Formik initialValues={initialValues} onSubmit={(values) => handleSubmit(values)}>
-      {() => (
-        <Form>
-          <div>Hello</div>
-        </Form>
-      )}
-    </Formik>
+    <Wrapper variant="small">
+      <Formik initialValues={initialValues} onSubmit={(values) => handleSubmit(values)}>
+        {({ values, handleChange }) => (
+          <Form>
+            <FormControl>
+              <FormLabel htmlFor="username">Username</FormLabel>
+              <Input value={values.username} id="username" placeholder="user..." onChange={handleChange} />
+              {/* <FormErrorMessage>Error</FormErrorMessage> */}
+            </FormControl>
+          </Form>
+        )}
+      </Formik>
+    </Wrapper>
   );
 }
 
