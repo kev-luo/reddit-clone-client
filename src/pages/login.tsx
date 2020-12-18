5a4520b005da667a0e5b9575d6faa0d3b6412dd1
Kevin Luo <kvn.luo@gmail.com>
2020-12-18 11:48:48 -0500

    create login page

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,7 +1,38 @@
+import React from "react";
+import { Formik, Form, FormikErrors } from "formik";
+import { Button } from "@chakra-ui/react";
+import { useRouter } from "next/router";
+
+import { InputField } from "../components/InputField";
+import { Wrapper } from "../components/Wrapper";
+import { toErrorMap } from "../utils/toErrorMap";
+
 interface loginProps {
 
 }
 
-export const login: React.FC<loginProps> = ({}) => {
-    return ();
+const Login: React.FC<loginProps> = ({ }) => {
+  const router = useRouter();
+  const initialValues = { username: "", password: "" };
+
+  const handleSubmit = async (values: { username: string; password: string; }) => {
+    console.log(values);
+    router.push("/login");
+  }
+
+  return (
+    <Wrapper variant="small">
+      <Formik initialValues={initialValues} onSubmit={(values) => handleSubmit(values)}>
+        {(props) => (
+          <Form>
+            <InputField name="username" label="Username"/>
+            <InputField name="password" label="Password" type="password" />
+            <Button type="submit" isLoading={props.isSubmitting}>Login</Button>
+          </Form>
+        )}
+      </Formik>
+    </Wrapper>
+  );
 }
+
+export default Login
