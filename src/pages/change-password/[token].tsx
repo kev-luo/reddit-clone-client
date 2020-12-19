9e5f755d8c4076a21d423398e3488767d1532796
Kevin Luo <kvn.luo@gmail.com>
2020-12-19 11:36:17 -0500

    change password page with getInitialProps

---
+++ src/pages/change-password/[token].tsx
@@ -0,0 +1,15 @@
+import { NextPage } from "next";
+
+const ChangePassword: NextPage<{ token: string }> = ({token}) => {
+    return (
+      <div>token is { token }</div>
+    );
+}
+
+ChangePassword.getInitialProps = ({ query }) => {
+  return {
+    token: query.token as string
+  }
+}
+
+export default ChangePassword;
