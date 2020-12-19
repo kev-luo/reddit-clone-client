baf0d9eafb0ccf2cf491d9e18c72dd6576d146fe
Kevin Luo <kvn.luo@gmail.com>
2020-12-19 18:56:57 -0500

    redirect to login if user goes to createpost page without being logged in

--- src/pages/create-post.tsx
+++ src/pages/create-post.tsx
@@ -1,8 +1,8 @@
 import { Button } from "@chakra-ui/react";
 import { Form, Formik } from "formik";
-import React from "react";
+import React, { useEffect } from "react";
 import { InputField } from "../components/InputField";
-import { useCreatePostMutation } from "../generated/graphql";
+import { useCreatePostMutation, useMeQuery } from "../generated/graphql";
 import { useRouter } from "next/router";
 import { withUrqlClient } from "next-urql";
 import { urqlClient } from "../utils/urqlClient";
@@ -10,15 +10,24 @@ import { Layout } from "../components/Layout";
 
 
 const CreatePost: React.FC<{}> = ({ }) => {
+  const [{ data, fetching }] = useMeQuery();
   const router = useRouter();
+  
+  useEffect(() => {
+    if(!fetching && !data?.me) {
+      router.replace("/login");
+    }
+  }, [fetching, data, router])
+
   const [, createPost] = useCreatePostMutation();
   const initialValues = {title: "", text: ""}
   return (
     <Layout variant="small">
       <Formik initialValues={initialValues} onSubmit={async (values) => {
-        const { error } = await createPost({ input: values });
-        console.log(error)
-        router.push("/");
+        const { error } = await createPost({ input: values })
+        if(!error) {
+          router.push("/");
+        }
       }}>
         {(props) => (
           <Form>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,23 @@
 import { withUrqlClient } from "next-urql"
-import { Navbar } from "../components/Navbar";
+import { Layout } from "../components/Layout";
 import { usePostsQuery } from "../generated/graphql";
 import { urqlClient } from "../utils/urqlClient"
+import { Link } from "@chakra-ui/react";
+import NextLink from "next/link";
 
 const Index = () => {
   const [{ data }] = usePostsQuery();
 
   return (
-    <>
-      <Navbar />
-      <div>Hello World</div>
+    <Layout>
+      <NextLink href="/create-post">
+        <Link>Create Post</Link>
+      </NextLink>
       <br />
       {!data ? <div>Loading...</div> : data.posts.map(post => (
         <div key={post.id}>{post.title}</div>
       ))}
-    </>
+    </Layout>
   );
 }
 
